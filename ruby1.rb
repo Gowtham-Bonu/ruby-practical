6dd8e06017ea738cdda86b1bd59fe0361a34e7ca
gowtham-bonu-simform <gowtham@simformsolutions.com>
2023-01-23 11:52:28 +0530

    TE7-T113:Introduction to Ruby:Feat:added changes based on the feedback

--- ruby1.rb
+++ ruby1.rb
@@ -1,39 +1,71 @@
 class Car                                                                     # A class named car which consists of relatable methods and data.
 
     N0_OF_WHEELS = 4  
-    
-    fuel_prices_h = Hash[:petrol => 105.4, :diesel => 96.72, :cng => 79.56 ]  # created a hash.
 
-    @@fuel_prices_e = fuel_prices_h.to_enum                                   # converted the created hash to an enum.
+    fuel_prices_h = Hash[:petrol => 105.4, :diesel => 96.72, :cng => 79.56 ]                                  # created a hash.
 
-     def gst_calculator(price)                                                # A method that takes in a parameter and caluculates the final price by imposing gst.
-        gst = (18.to_f/100)*price
-        tot_price = (price + gst).to_i 
-        return tot_price
+    @@fuel_prices_e = fuel_prices_h.to_enum                                                                   # converted the created hash to an enum.
+
+    def gst_calculator                                                                                       # A method that takes in a parameter and caluculates the final price by imposing gst.
+        
+        begin
+            p "Enter a number that's greater than 0"
+            @price = gets.chomp.to_i
+        end  until @price.is_a? Integer and @price>0
+        
+        gst = (18.to_f/100)*@price
+        tot_price = (@price + gst).to_i 
+        p "the final price is #{tot_price}"
     end
 
-    def scope_tester(color_of_obj = "red")                                    # A method which creates 2 type of variables and checks their scope and prints the same.
-        color = color_of_obj
-        @color = color_of_obj
-        p "scope of color : #{defined?(color)}"
-        p "scope of @color : #{defined?(@color)}"
+    def scope_tester                                                                                          # A method which creates 2 type of variables and checks their scope and prints the same.
+        color = "red"
+        @color = "blue"
+
+        scope_tester_imp
     end
+
+    def scope_tester_imp
+        if @color
+            p "'@color' is available in the scope of method 'scope_tester_imp'"
+        else
+            p "'@color' is'nt available in the scope of method 'scope_tester_imp'"
+        end
+
+        if color                                                                                             # calling a local variable form another method returns an error.
+            p "'color' is available in the scope of method 'scope_tester_imp'"
+        else
+            p "'color' is'nt available in the scope of method 'scope_tester_imp'"
+        end
+    end
+
+
 
     def how_many_wheels                                                       # This method uses the CONSTANT variable.
         p "this vehicle is a #{N0_OF_WHEELS} wheeler."
     end
 
-    def info_color(color="white")                                                     # Takes in a parameter and prints it.
-        p "the color of the car is #{color}"
+    def info_color                                                            # Takes in a parameter and prints it.
+        begin
+            p "Give me a color"
+            color = gets.chomp
+        end  until color.is_a? String
+
+        p "The color of the car is : #{color}"
+        
     end
 
     def info_fuel_prices                                                      # This method uses the enum
         @@fuel_prices_e.with_index{ |fuel_price| p fuel_price }
     end
-
-
-
 end
+
+
+
+
+car = Car.new
+car.scope_tester
+
 
 =begin  --tests--
 car = Car.new
